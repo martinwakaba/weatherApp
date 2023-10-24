443326016705a0874d055342074d063deba9d006
MartinWakaba <martin.wakaba@student.moringaschool.com>
2023-10-24 21:06:58 +0300

    making dynamic changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded',() =>{
-    getWeather('Nairobi');
+    getWeather('Arusha');
 
 })
 const apikey = '1db2821680c55cae961eb3354269fb98';
@@ -9,8 +9,26 @@ function getWeather(city){
     fetch(apiUrl + city + `&appid=${apikey}`)
     .then(res => res.json())
     .then(data => {
-        const area = document.querySelector
+        const areaCode = document.querySelector('#areacode')
+        const input = document.querySelector('#city-input');
+        const temperature = document.querySelector('#temperature');
+        const condition = document.querySelector('#conditiont');
+        const search = document.querySelector('#search');
+
+        areaCode.innerHTML = data.name;
+
+        
 
     })
     .catch(error => error.log('Error', Error))
 }
+
+
+
+
+
+
+
+
+
+
