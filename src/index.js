2c22ed689e84210ec12d423dc6629bc7c7e12c47
MartinWakaba <martin.wakaba@student.moringaschool.com>
2023-10-24 21:30:47 +0300

    changes in js and html

--- src/index.js
+++ src/index.js
@@ -1,21 +1,32 @@
 document.addEventListener('DOMContentLoaded',() =>{
-    getWeather('Arusha');
+    getWeather('Nairobi');
 
 })
 const apikey = '1db2821680c55cae961eb3354269fb98';
 const apiUrl = `https://api.openweathermap.org/data/2.5/weather?&=&units=metric&q=`;
 
+
+        const areaCode = document.querySelector('#areacode')
+        const cityName = document.querySelector('#city-name')
+        const humidity = document.querySelector('#humidity')
+        const wind = document.querySelector('#wind')
+        const input = document.querySelector('#city-input');
+        const temperature = document.querySelector('#temperature');
+        const condition = document.querySelector('#weather-condition');
+        const search = document.querySelector('#search');
+
 function getWeather(city){
     fetch(apiUrl + city + `&appid=${apikey}`)
     .then(res => res.json())
     .then(data => {
-        const areaCode = document.querySelector('#areacode')
-        const input = document.querySelector('#city-input');
-        const temperature = document.querySelector('#temperature');
-        const condition = document.querySelector('#conditiont');
-        const search = document.querySelector('#search');
+        console.log(data);
 
         areaCode.innerHTML = data.name;
+        temperature.innerHTML = Math.round(data.main.temp) + "°c";
+        humidity.innerHTML = data.main.humidity + '%';
+        wind.innerHTML = data.wind.speed + 'Km/h';
+        condition.innerHTML = data.weather[0].main;
+        cityName.innerHTML = data.name;
 
         
 
